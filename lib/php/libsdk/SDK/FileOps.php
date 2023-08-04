0eb5ef6754a8c611697a18553a927b6bfd04f78d
Ezzalddeen Ali <eng.dev.ezz@gmail.com>
2023-08-05 02:51:53 +0300

    Update FileOps.php

--- lib/php/libsdk/SDK/FileOps.php
+++ lib/php/libsdk/SDK/FileOps.php
@@ -132,8 +132,9 @@ retry:
 
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, CURL_SSLVERSION_MAX_TLSv1_2);
 		curl_setopt($ch, CURLOPT_USERAGENT, Config::getSdkUserAgentName());
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 
